ab0383e8e7d10a1fa2107c257b83e289e066cefa
Renat <renat.sakenov@gmx.de>
2021-08-15 13:25:45 +0200

    takes user input and displays counter of total and wrong answers

--- lang_num_exercise.py
+++ lang_num_exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Description:
-#  Says a number outloud. When pressing a key, displays the number.
+#  Says a number outloud and takes the users answer. When pressing a key, displays the number and counter of total and wrong answers.
 #
 # Dependencies (pip modules):
 #   gTTS playsound
@@ -32,14 +32,21 @@ def LangNumExercise():
     _ = os.system(CLS_CMD)
     print("Start of language number exercise.")
     action()
+    total_answers = 0
+    wrong_answers = 0
     while True:
         num = randint(num_range[0], num_range[1])
         tts = gtts.gTTS(str(num), lang=language)
         tts.save(fileName)
         playsound(fileName)
-        input('\nPress enter to display the number\n')
+        user_input = int(input('\nInput the number: \n'))
+        if user_input != num:
+          wrong_answers += 1
+        total_answers += 1
         _ = os.system(CLS_CMD)
-        print("\n%s\n" % str(num))
+        print("\nThe number is: %s.\nYour answer was: %s.\n" % (str(num),str(user_input)))
+        print("\n\nTotal answers: %s\n" % str(total_answers))
+        print("Wrong answers: %s\n" % str(wrong_answers))
         action()
 
 
